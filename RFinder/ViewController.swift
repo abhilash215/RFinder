bb8e2c4d772370ce0935ac67b0890e73def0754f
abhilash uday <abhilashuday@abhilashs-mbp.fios-router.home>
2016-10-28 01:29:46 -0400

    added the test message

--- RFinder/ViewController.swift
+++ RFinder/ViewController.swift
@@ -13,12 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("testing the working of git on my local")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
